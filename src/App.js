6b7a49a9f2409f7d4327e7cd511016cb55bdac2d
shafay-azeem <shafayazeem12@gmail.com>
2023-04-03 16:44:01 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// import Router
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// import Component
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import ScrollToTop from "./component/ScrollToTop";
+
+// import Pages
+import Home from "./Pages/Home";
+import ReactHooks from "./Pages/ReactHooks";
+import UiUx from "./Pages/UiUx";
+import FormExample from "./Pages/FormExample";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <Router>
+        <Header />
+        <ScrollToTop>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/react-hooks" component={ReactHooks} />
+            <Route exact path="/ui-ux" component={UiUx} />
+            <Route exact path="/form-example" component={FormExample} />
+          </Switch>
+        </ScrollToTop>
+        <Footer />
+      </Router>
+    </React.StrictMode>
   );
 }
 
