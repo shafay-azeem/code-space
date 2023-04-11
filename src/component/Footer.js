06799e8eadc48c9829f61746d08e9fc4a04a51c4
faizan-99 <mfaizy605@gmail.com>
2023-04-11 15:35:16 +0500

    updated

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -17,74 +17,62 @@ function Footer() {
       >
         <div className="about-social-icon text-center">
           <ul className="about-social">
-            <li className="wow fadeIn" data-wow-delay=".8s">
+            {/* <li className="wow fadeIn" data-wow-delay=".8s">
               <a
-              // href="https://twitter.com/sanajitjana01"
-              //  target="newtab"
+              href="https://twitter.com/sanajitjana01"
+               target="newtab"
               >
                 <i className="fa fa-twitter" aria-hidden="true"></i>
               </a>
-            </li>
+            </li> */}
             <li className="wow fadeIn" data-wow-delay=".6s">
               <a
-              // href="https://www.linkedin.com/in/sanajitjana01"
-              // target="newtab"
+                href="https://www.linkedin.com/in/code-space-501a83271"
+                target="newtab"
               >
                 <i className="fa fa-linkedin" aria-hidden="true"></i>
               </a>
             </li>
             <li className="wow fadeIn" data-wow-delay=".2s">
               <a
-              // href="https://www.facebook.com/sanajitjana01/"
-              // target="newtab"
+                href="https://www.facebook.com/CodeSpaceYem/?mibextid=ZbWKwL"
+                target="newtab"
               >
                 <i className="fa fa-facebook" aria-hidden="true"></i>
               </a>
             </li>
             <li className="wow fadeIn" data-wow-delay=".4s">
               <a
-              // href="https://www.instagram.com/sanajitjana01/"
-              // target="newtab"
+                href="https://instagram.com/codespaceyem?igshid=ZDdkNTZiNTM="
+                target="newtab"
               >
                 <i className="fa fa-instagram" aria-hidden="true"></i>
               </a>
             </li>
-            <li className="wow fadeIn" data-wow-delay="1s">
+            {/* <li className="wow fadeIn" data-wow-delay="1s">
               <a
-              // href="https://sanajitjana.medium.com/"
-              // target="newtab"
+              href="https://sanajitjana.medium.com/"
+              target="newtab"
               >
                 <i className="fa fa-medium" aria-hidden="true"></i>
               </a>
-            </li>
-            <li className="wow fadeIn" data-wow-delay="1.2s">
+            </li> */}
+            {/* <li className="wow fadeIn" data-wow-delay="1.2s">
               <a
-              // href="https://github.com/sanajitjana"
-              // target="newtab"
+              href="https://github.com/sanajitjana"
+              target="newtab"
               >
                 <i className="fa fa-github" aria-hidden="true"></i>
               </a>
-            </li>
+            </li> */}
           </ul>
         </div>
         <div className="copyright">
-          {/* <h3>&copy; {currYear}, Made with by Sanajit Jana.</h3> */}
           <h3 style={{ color: "white" }}>
             {t("Copyright")} &copy;{" "}
             <a style={{ color: "white" }}>{t("CodeSpace")}</a>
           </h3>
         </div>
-        {/* <a
-          href="//www.dmca.com/Protection/Status.aspx?ID=4ce89e88-486f-4e19-9159-e412a842118e"
-          title="DMCA.com Protection Status"
-          class="dmca-badge"
-        >
-          <img
-            src="https://images.dmca.com/Badges/dmca_protected_sml_120n.png?ID=4ce89e88-486f-4e19-9159-e412a842118e"
-            alt="DMCA.com Protection Status"
-          />
-        </a> */}
-        {/* <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"></script> */}
       </div>
     </React.StrictMode>
   );
